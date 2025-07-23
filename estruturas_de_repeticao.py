0df7c248855f82c5030bbb48c59c3d0697e5b79c
Joel T Cunha <joel.cunha@educacao.mg.gov.br>
2025-07-23 19:28:25 -0300

    desafio descontos

--- estruturas_de_repeticao.py
+++ estruturas_de_repeticao.py
@@ -1,3 +1,5 @@
+from desafio_operacao_bancaria import valor_deposito
+
 texto = input("Informe um texto: ")
 VOGAIS = "AEIOU"
 
@@ -11,3 +13,25 @@ print()
 # RANGE, início, fim, step
 for numero in range(0, 51, 5):
     print(numero, end=" - ")
+
+descontos = {
+    "DESCONTO10": 0.10,
+    "DESCONTO20": 0.20,
+    "SEM_DESCONTO": 0.00
+}
+
+# Entrada do usuário
+preco = float(input().strip())
+cupom = input().strip()
+
+# TODO: Aplique o desconto se o cupom for válido:
+if cupom == "DESCONTO10":
+   valor_produto = preco - (preco * 10/100)
+   print(valor_produto)
+
+elif cupom == "DESCONTO20":
+    valor_produto = preco - (preco * 20 / 100)
+    print(valor_produto)
+
+elif cupom == "SEM_DESCONTO":
+    print(preco)
